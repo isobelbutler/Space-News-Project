9643d913c3a7766e8e0418d0ae696b800681dc80
GeorgeKlemperer <george@klemperer.co.uk>
2023-06-16 15:23:34 +0100

    Guardian api added

--- script.js
+++ script.js
@@ -1,67 +1,105 @@
-let placeholderImage = 'images/image-placeholder.png';
-document.getElementById("user-image-container").style.background = `url(${placeholderImage}) center / cover no-repeat`;
+let placeholderImage = "images/image-placeholder.png";
+document.getElementById(
+  "user-image-container"
+).style.background = `url(${placeholderImage}) center / cover no-repeat`;
 
 const NASAapiKey = "3wnxOfRJD4eT7ejeRvcgQUIIKAANUyqtIBVq62Iz";
-const unsplashAPIkey = "UhChBZg41aaNCvsaG2V9bstJAN_MB7U8UuVkqoOtDJQ"
+const unsplashAPIkey = "UhChBZg41aaNCvsaG2V9bstJAN_MB7U8UuVkqoOtDJQ";
 const form = document.querySelector("form");
 
 const apodDisplayElement = document.getElementById("user-image-container");
 const apodDescriptionElement = document.getElementById("explanation");
-
+const apiHeadlineElement = document.getElementById("apiHeadline");
 
 form.addEventListener("submit", (event) => {
-    event.preventDefault();
-  
-    const dateInput = document.getElementById("formDate").value;
-    const apiUrl = `https://api.nasa.gov/planetary/apod?api_key=${NASAapiKey}&date=${dateInput}`;
+  event.preventDefault();
 
-    console.log(document.getElementById("date").value)
-  
-    // Show loading wheel
-    apodDisplayElement.style.background = `url(https://upload.wikimedia.org/wikipedia/commons/5/53/Loading-red-spot.gif) center / cover no-repeat`;  
-    fetch(apiUrl)
-      .then((response) => {
-        if (response.ok) {
-          return response.json();
-        } else {
-          throw new Error('Network response was not OK.');
-        }
-      })
-      .then((data) => {
-        const hdUrl = data.hdurl;
-        const description = data.explanation;
-  
-        if (hdUrl === undefined) {
-          throw new Error('HD URL is undefined.');
-        }
-  
-        apodDisplayElement.style.background = `url(${hdUrl}) center / cover no-repeat`;  
-        // can background image have an alt?
-        // apodDisplayElement.alt = description;
-        apodDescriptionElement.textContent = description;
-  
-      })
-      .catch((error) => {
-        console.log("Error:", error);
-        
-        // Fetch a random space-related image
-        fetch(`https://api.unsplash.com/photos/random?query=space&orientation=landscape&client_id=${unsplashAPIkey}`)
-          .then((response) => response.json())
-          .then((data) => {
-            const randomImageUrl = data.urls.regular;
-            
-            // Display the random space-related image
-            apodDisplayElement.style.background = `url(${randomImageUrl}) center / cover no-repeat`;
+  const dateInput = document.getElementById("formDate").value;
+  const apiUrl = `https://api.nasa.gov/planetary/apod?api_key=${NASAapiKey}&date=${dateInput}`;
 
-            // apodDisplayElement.alt = "Random Space Image";
-            apodDescriptionElement.textContent = "Houston, we've had a problem... randomly generated space-related image (no NASA astrology picture of the day for this date)";
-  
-            console.log("Random Space Image URL:", randomImageUrl);
-          })
-          .catch((error) => {
-            console.log("Error fetching random space image:", error);
-          });
-      });
-  });
-  
-  // nb a good date to test an undefined apod hdurl response is 06/06/23 and 31/05/2023
+  // Show loading wheel
+  apodDisplayElement.style.background = `url(https://upload.wikimedia.org/wikipedia/commons/5/53/Loading-red-spot.gif) center / cover no-repeat`;
+  fetch(apiUrl)
+    .then((response) => {
+      if (response.ok) {
+        return response.json();
+      } else {
+        throw new Error("Network response was not OK.");
+      }
+    })
+    .then((data) => {
+      const hdUrl = data.hdurl;
+      const description = data.explanation;
+
+      if (hdUrl === undefined) {
+        throw new Error("HD URL is undefined.");
+      }
+
+      apodDisplayElement.style.background = `url(${hdUrl}) center / cover no-repeat`;
+      apodDescriptionElement.textContent = description;
+    })
+    .catch((error) => {
+      console.log("Error:", error);
+
+      // Fetch a random space-related image
+      fetch(
+        `https://api.unsplash.com/photos/random?query=space&orientation=landscape&client_id=${unsplashAPIkey}`
+      )
+        .then((response) => response.json())
+        .then((data) => {
+          const randomImageUrl = data.urls.regular;
+
+          // Display the random space-related image
+          apodDisplayElement.style.background = `url(${randomImageUrl}) center / cover no-repeat`;
+
+          // apodDisplayElement.alt = "Random Space Image";
+          apodDescriptionElement.textContent =
+            "Houston, we've had a problem... randomly generated space-related image (no NASA astrology picture of the day for this date)";
+
+          console.log("Random Space Image URL:", randomImageUrl);
+        })
+        .catch((error) => {
+          console.log("Error fetching random space image:", error);
+        });
+    });
+});
+// nb a good date to test an undefined apod hdurl response is 06/06/23 and 31/05/2023
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const formData = new FormData(form);
+  const articleFilter = document.getElementById("articleSection").value.toLowerCase();
+  const articleFilterURLAppendage =
+    articleFilter === "general"
+      ? "" : `&order-by=relevance&section=${articleFilter}`;
+
+  const specificDate = document.getElementById("formDate").value;
+
+  const fetchArticlesByDateAndSection = async (date, section) => {
+    const apiKey = "bb4717f4-9ef8-4141-a00c-6cf38e5d80e4";
+    const url = `https://content.guardianapis.com/search?from-date=${specificDate}&to-date=${specificDate}&api-key=${apiKey}${articleFilterURLAppendage}`;
+
+    console.log(url);
+    console.log(specificDate);
+    console.log(articleFilter);
+
+    try {
+      const response = await fetch(url);
+      const data = await response.json();
+      const headline = data.response.results[0].webTitle;
+      const headlineUrl = data.response.results[0].webUrl; // New line: Get the URL of the headline story
+      apiHeadlineElement.innerText = headline;
+      apiHeadlineElement.innerHTML = `<a href="${headlineUrl}" target="_blank">${headline}</a>`; // Modified line: Attach the URL to the generated headline as a hyperlink
+
+      document.querySelector(".headline-text").style.display = "initial";
+
+      
+
+    } catch (error) {
+      console.log("Error:", error);
+    }
+  };
+
+  fetchArticlesByDateAndSection(specificDate, articleFilter);
+});
+// Good break case = business 14/06/23
